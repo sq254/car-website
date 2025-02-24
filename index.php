a62418c0fd844af00f8a487e0206fea670078326
sq254 <franklinemokaya123@gmail.com>
2025-02-24 13:02:32 +0300

    Update project with latest changes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <div class="container" style="text-align: center;">
     <!-- Visible title on the page -->
     <h1 class="site-title">Welcome to Car Rental & Hire Hub</h1>
-    <p style="color: black;">Please choose one of the following options:</p>
+    <p style="color: white;">Please choose one of the following options:</p>
     <a href="/carrental/rental_cars.php" class="btn" style="background-color: black; color: white;">Rental</a>
     <a href="/carrental/purchase_cars.php" class="btn" style="background-color: black; color: white;">Purchase</a>
   </div>
